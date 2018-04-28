6c14446d8e612e3cc2a550c39cd86563105a3d4f
Mary-Kate James <marykatj@gmail.com>
2018-04-28 19:34:23 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,33 +4,32 @@ end
 
 def create_an_array
   beagles_nicknames = ["Cin", "Cinna_Monster", "Cin-City", "Cinnister"]
-
 end
 
 def add_element_to_end_of_array(array, element)
-
+  beagles_nicknames.push("Cinnnaminnnamon")
 end
 
 def add_element_to_start_of_array(array, element)
-
+  
 end
 
 def remove_element_from_end_of_array(array)
-
+  
 end
 
 def remove_element_from_start_of_array(array)
-
+  
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  
 end
 
 def retrieve_first_element_from_array(array)
-
+  
 end
 
 def retrieve_last_element_from_array(array)
-
+  
 end
